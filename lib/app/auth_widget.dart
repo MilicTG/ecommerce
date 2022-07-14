f68063ba10558c013d95f445cb16626f79712e7c
MilicTG <milic.tg@gmail.com>
2022-07-14 13:28:01 +0200

    Chapter 9.7

---
+++ lib/app/auth_widget.dart
@@ -0,0 +1,35 @@
+import 'package:ecommerce/app/providers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthWidget extends ConsumerWidget {
+  final WidgetBuilder nonSignedInBuilder;
+  final WidgetBuilder signedInBuilder;
+
+  const AuthWidget(
+      {Key? key,
+      required this.signedInBuilder,
+      required this.nonSignedInBuilder})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authStateChanges = ref.watch(authStateChangesProvider);
+
+    return authStateChanges.when(
+      data: (user) =>
+          user != null ? signedInBuilder(context) : nonSignedInBuilder(context),
+      loading: () => const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+      error: (_, __) => const Scaffold(
+        body: Center(
+          child: Text("Something went wrong!"),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,47 @@
-import 'package:ecommerce/firebase_options.dart';
+import 'package:ecommerce/app/auth_widget.dart';
+import 'package:ecommerce/app/pages/auth/sign_in_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'app/providers.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'Ecommerce Flutter',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: AuthWidget(
+        signedInBuilder: (context) => Scaffold(
+          body: Center(
+              child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text("Signed In in"),
+              ElevatedButton(
+                  onPressed: () {
+                    ref.read(firebaseAuthProvider).signOut();
+                  },
+                  child: const Text("Sign out"))
+            ],
+          )),
+        ),
+        nonSignedInBuilder: (_) => const SignInPage(),
+      ),
     );
   }
 }
